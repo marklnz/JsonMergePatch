3aeb6175d5a2c6e8e9ab9d130a4d5c2dd5deae9f
Mark Lawrence <mark.lawrence@eyede.co.nz>
2021-06-01 13:40:32 -0700

    Initial commit

---
+++ src/JsonMergePatch.Console/JsonMergePatch.cs
@@ -0,0 +1,79 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+
+namespace JsonMergePatch
+{
+    public static class JsonMergePatch
+    {
+        public static string Patch(object target, string patch)
+        {
+            return Patch(JsonSerializer.Serialize(target), patch);
+        }
+
+        public static string Patch(string target, string patch)
+        {
+            var options = new JsonReaderOptions
+            {
+                AllowTrailingCommas = true,
+                CommentHandling = JsonCommentHandling.Skip
+            };
+
+            var targetDoc = JsonDocument.Parse(target);
+            var patchDoc = JsonDocument.Parse(patch);
+
+            return DoPatch(targetDoc.RootElement, patchDoc.RootElement);
+        }
+
+        private static string DoPatch(JsonElement targetElement, JsonElement patchElement, string name = null)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if (patchElement.ValueKind == JsonValueKind.Object)
+            {
+                if (targetElement.ValueKind != JsonValueKind.Object)
+                {
+                    sb.Append("{}");
+                }
+
+                sb.Append("{");
+
+                foreach (var property in patchElement.EnumerateObject())
+                {
+                    var propExistsOnTarget = targetElement.TryGetProperty(property.Name, out var targetProp);
+
+                    // The value 1 here is the length of the start object token ({), which we've added above. 
+                    // If the string so far is any longer than that then it means we've already written at least one property and we need to 
+                    // add a comma as a separator
+                    if (sb.Length > 1)
+                        sb.Append(",");
+
+                    if (property.Value.ValueKind == JsonValueKind.Null)
+                    {
+                        if (propExistsOnTarget)
+                        {
+                            // Remove from target (i.e. do NOT write out to writer)
+                        }
+                    }
+                    else
+                    {
+                        // Pass the current value of property, and the relevant target element to DoPatch
+                        var val = DoPatch(targetProp, patchElement.GetProperty(property.Name), property.Name);
+                        // write val
+                        sb.Append($"\"{property.Name}\":{val}");
+                    }
+                }
+
+                sb.Append("}");
+            }
+            else
+            {
+                // write the patch value
+                return patchElement.GetRawText();
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ src/JsonMergePatch.Console/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Text.Json;
+
+namespace JsonMergePatch
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("JsonMergePatch console application");
+
+            // TODO: Create an instance of an object, which we will use to test the patcher...
+            var test = new TestObject()
+            {
+                Id = 1,
+                Name = "A Test",
+                HasBeenPatched = false,
+                TimeOfPatch = DateTimeOffset.MinValue,
+                TestArray = new TestChildObject[]
+                {
+                    new TestChildObject() { Index = 1, Name = "Test Child 1" },
+                    new TestChildObject() { Index = 2, Name = "Test Child 2"}
+                }
+            };
+
+            var prePatchJson = JsonSerializer.Serialize(test);
+
+            Console.WriteLine($"Patch Object before patch application: {prePatchJson}");
+            Console.WriteLine();
+
+            // TODO: Then create a patch json for it
+            var patch = new TestObject()
+            {
+                Id = 2,
+                Name = "A Patched Test",
+                HasBeenPatched = true,
+                TimeOfPatch = DateTimeOffset.Now,
+                TestArray = new TestChildObject[]
+                {
+                    new TestChildObject() { Index = 1, Name = "Test Child 1" },
+                    new TestChildObject() { Index = 3, Name = "Test Child 3"}
+                }
+            };
+
+            var patchJson = JsonSerializer.Serialize(patch);
+
+            Console.WriteLine($"Patch Json: {patchJson}");
+            Console.WriteLine();
+
+            // TODO: Then run through the patcher.
+            var postPatchJson = JsonMergePatch.Patch(prePatchJson, patchJson);
+
+            TestObject patched = JsonSerializer.Deserialize<TestObject>(postPatchJson);
+
+            // TODO: Test that the changes worked.
+            //var postPatchJson = JsonSerializer.Serialize(test);
+            Console.WriteLine($"Patch Object after patch application: {postPatchJson}");
+            Console.WriteLine();
+
+            if (postPatchJson == patchJson)
+                Console.WriteLine("SUCCEEDED! Patched object json matches patch json");
+            else
+                Console.WriteLine("FAILED! Patched object json different from patch json");
+
+            if (patched.Id == patch.Id && patched.Name == patched.Name && patched.HasBeenPatched == patch.HasBeenPatched &&
+                patched.TimeOfPatch == patch.TimeOfPatch && patched.TestArray.Length == patch.TestArray.Length &&
+                patched.TestArray.Length == 2 && patched.TestArray[0].Index == patch.TestArray[0].Index &&
+                patched.TestArray[0].Name == patch.TestArray[0].Name && patched.TestArray[1].Index == patch.TestArray[1].Index &&
+                patched.TestArray[1].Name == patch.TestArray[1].Name)
+            {
+                Console.WriteLine("SUCCEEDED! Deserialized object was patched correctly");
+            }
+            else
+            {
+                Console.WriteLine("FAILED! Deserialized object was not patched correctly");
+            }
+        }
+    }
+
+    public class TestObject
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool HasBeenPatched { get; set; }
+        public DateTimeOffset TimeOfPatch { get; set; }
+        public TestChildObject[] TestArray { get; set; }
+    }
+
+    public class TestChildObject
+    {
+        public int Index { get; set; }
+        public string Name { get; set; }
+    }
+}
